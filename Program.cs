bee086518e5dcca989cc8453171bbd8aa7ba41d1
Tim Reilly <timreilly@live.com>
2017-07-20 14:10:47 -0700

    continuing with delegate composition

--- Program.cs
+++ Program.cs
@@ -1,45 +1,48 @@
 ﻿using System;
+using System.Threading; 
+using System.Runtime.InteropServices.ComTypes;
 
 namespace DelegatePractice
 {
 
-    public delegate string FirstDelegate(int x); 
-    class DelegateTest
+    delegate void MyDelegate(string s); 
+    class MyClass
     {
-        string name; 
+        public static void Hello(string s){
+            Console.WriteLine("   Hello, {0}", s); 
+        }
+
+        public static void Goodbye(string s){
+            Console.WriteLine("  Goodbye, {0}!", s); 
+        }
+
+
 
         // public delegate void SecondDelegate(char a, char b); 
         static void Main(string[] args)
         {
-            
-            DelegateTest t= new DelegateTest(); 
+            MyDelegate a, b, c, d; 
 
-            t.MyEvent += new EventHandler(t.DoNothing); 
-            t.MyEvent -= null; 
+            // Create the delegate objest a that references the method Hello:
+            a = new MyDelegate(Hello); 
+            b = new MyDelegate(Goodbye); 
 
-        }
+            // The two delegates, a and b, are compsed to form c: 
+            c = a + b; 
 
-        static string StaticMethod(int i){
-            return string.Format ("Static method: {0}", i);  
-        }
+            d = c - a; 
 
-        string InstanceMethod(int i){
-            return string.Format("{0}: {1}", name, i); 
-        }
-
-        public event EventHandler MyEvent
-        {
-            add {
-                Console.WriteLine("Add operation"); 
-            }
-
-            remove {
-                Console.WriteLine ("remove operation"); 
-            }
-        }
-
-        void DoNothing (object sender, EventArgs e){
+            Console.WriteLine("Invoking delegate a ");
+            a("A"); 
+            Console.WriteLine("Invoking delegate b ");
+            b("B");
+            Console.WriteLine("Invoking delegate c ");
+            c("C"); 
+            Console.WriteLine("Invoking delegate d ");
+            d("D");
 
         }
     }
 }
+            
+
