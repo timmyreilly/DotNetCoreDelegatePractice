132adfa6dd77396f0c05062c8d77e1a9775e8c28
Tim Reilly <timreilly@live.com>
2017-07-20 12:24:59 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DelegatePractice
+{
+
+    public delegate string FirstDelegate(int x); 
+    class DelegateTest
+    {
+        string name; 
+
+        // public delegate void SecondDelegate(char a, char b); 
+        static void Main(string[] args)
+        {
+            
+            FirstDelegate d1 = new FirstDelegate(DelegateTest.StaticMethod);
+
+            DelegateTest instance = new DelegateTest(); 
+            instance.name = "My Intance";
+            FirstDelegate d2 = new FirstDelegate(instance.InstanceMethod); 
+
+            Console.WriteLine(d1(10)); 
+            Console.WriteLine(d2(5));
+
+        }
+
+        static string StaticMethod(int i){
+            return string.Format ("Static method: {0}", i);  
+        }
+
+        string InstanceMethod(int i){
+            return string.Format("{0}: {1}", name, i); 
+        }
+    }
+}
