65032c5b0dd85daf329fbe0a2f120187001e0be9
Tim Reilly <timreilly@live.com>
2017-07-24 18:20:04 -0700

    using get invocation list

--- ProgramFive.cs
+++ ProgramFive.cs
@@ -1,23 +1,50 @@
 using System;
+using System.Collections.Generic; 
 
 namespace DelegatePricate
 {
 
     delegate void MeDelegate();
-
+    delegate int AnotherDelegate(); 
     class MainClassTwo
     {
-        static void Main()
+        static void MainA()
         {
             MeDelegate d = Foo;
-            d = (MeDelegate)Delegate.Combine(d, new MeDelegate(Goo)); 
-            d += Goo; 
+            d = (MeDelegate)Delegate.Combine(d, new MeDelegate(Goo));
+            d += Goo;
             d += Sue;
-            d(); 
+            d();
         }
 
         static void Foo() { Console.WriteLine("Foo()"); }
         static void Goo() { Console.WriteLine("Goo()"); }
         static void Sue() { Console.WriteLine("Sue()"); }
+
+        static void Main()
+        {
+            AnotherDelegate d = ReturnFive; 
+            d += ReturnTen; 
+            d += ReturnTwentyTwo; 
+            // int value = d(); 
+
+            List<int> ints = new List<int>(); 
+            foreach(AnotherDelegate del in d.GetInvocationList())
+                ints.Add(del()); 
+
+            foreach(int i in ints){
+                Console.WriteLine(i);
+            }
+            //Console.WriteLine(value); 
+
+
+        }
+
+        static int ReturnFive() { return 5; }
+        static int ReturnTen() {return 10; }
+        static int ReturnTwentyTwo() {return 22; }
+        
     }
+
+
 }
