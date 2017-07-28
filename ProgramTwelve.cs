c9640ca5a90cfea0a87210fde83340e961c493a9
Tim Reilly <timreilly@live.com>
2017-07-28 12:38:00 -0700

    static extension methods

--- ProgramTwelve.cs
+++ ProgramTwelve.cs
@@ -1,38 +1,49 @@
-using System; 
+using System;
 
-static class MainTwelve{
+static class MainTwelve
+{
     // Extension methods! 
-    static DateTime Combine(this DateTime datePart, DateTime timePart){
+    static DateTime Combine(this DateTime datePart, DateTime timePart)
+    {
         return new DateTime(
-            datePart.Year, datePart.Month, datePart.Day, 
+            datePart.Year, datePart.Month, datePart.Day,
             timePart.Hour, timePart.Minute, timePart.Second);
     }
 
-    static void MainA(){
-        DateTime date = DateTime.Parse("1/5/2025"); 
-        DateTime time = DateTime.Parse("1/1/0001 9:55"); 
-        DateTime combined = Combine(date, time); 
-        DateTime combined2 = date.Combine(time); 
+    static void MainA()
+    {
+        DateTime date = DateTime.Parse("1/5/2025");
+        DateTime time = DateTime.Parse("1/1/0001 9:55");
+        DateTime combined = Combine(date, time);
+        DateTime combined2 = date.Combine(time);
 
-        Console.WriteLine($"{combined} or {combined2}"); 
+        Console.WriteLine($"{combined} or {combined2}");
     }
 
-    class Cow{
-        int numMoos; 
-        public static void Moo(Cow _this){
-            _this.numMoos++;
-            Console.WriteLine("Mooooo" +_this.numMoos); 
-        }
-    }
+    static void Main()
+    {
+        Cow2 c1 = new Cow2();
+        Cow2 c2 = new Cow2();
 
-    static void Main(){
-        Cow c1 = new Cow(); 
-        Cow c2 = new Cow(); 
-
-        Cow.Moo(c1); 
-        Cow.Moo(c2); 
-        Cow.Moo(c2); 
-        Cow.Moo(c2); 
+        c1.Moo();
+        c1.Moo();
+        c1.Moo();
+        c2.Moo();
         
+
+    }
+}
+// static extension methods 
+class Cow2
+{
+    public int numMoos;
+}
+
+static class CowMethods
+{
+    public static void Moo(this Cow2 _this)
+    {
+        _this.numMoos++;
+        Console.WriteLine("Mooooo" + _this.numMoos);
     }
 }
