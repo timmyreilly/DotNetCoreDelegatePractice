7b5b9fa7ba49eae122c1013abc87d3593f7684a9
Tim Reilly <timreilly@live.com>
2017-07-28 09:21:00 -0700

    covariance practice

--- ProgramTen.cs
+++ ProgramTen.cs
@@ -10,9 +10,32 @@ class MainTen{
     static void TakeDerived(Derived d){}
     static void TakeBase(Base b){}
 
-    static void Main(){
+    static void MainA(){
         MyOtherDelegate d1 = TakeDerived; 
         MyOtherDelegate d2 = TakeBase;
         d2(new Derived()); 
     }
+}
+
+
+// Covariance work - the return types are covariant - 
+delegate Base ReturnsBaseDelegate(); 
+delegate Derived ReturnsDerivedDelegate(); 
+
+class MainTen2{
+    static Base ReturnsBase() { return null; }
+    static Derived ReturnsDerived() { return null; } 
+
+
+    static void Main()
+    {
+        ReturnsBaseDelegate b; 
+        b = ReturnsBase; 
+        b = ReturnsDerived; 
+
+        ReturnsDerivedDelegate d; 
+        // d = ReturnsBase; derived is a base, base is not 'derive' 
+        d = ReturnsDerived; 
+    }
+
 }
